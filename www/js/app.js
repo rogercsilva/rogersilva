881790b480d70ce1cdc4be53a630a87082eb9824
Roger Silva <roger.silva@al.infnet.edu.br>
2017-10-09 12:48:34 +0000

    acho que esta indo

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,14 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -32,42 +24,54 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.cachorros', {
+    url: '/cachorros/:id',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/listaanimais.html',
+        controller: 'CachorrosCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.gatos', {
+      url: '/gatos',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/listaanimais.html',
+          controller: 'GatosCtrl'
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
+    .state('app.perfil', {
+      url: '/perfil',
       views: {
         'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+          templateUrl: 'templates/perfil.html',
+          controller: 'PerfilCtrl'
         }
       }
     })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
+    
+    .state('app.cachorros', {
+    url: '/cachorros/:id',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/animal.html',
+        controller: 'CachorrosCtrl'
       }
     }
-  });
+  })
+
+  .state('app.gatos', {
+      url: '/gatos',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/listaanimais.html',
+          controller: 'GatosCtrl'
+        }
+      }
+    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/gatos');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,63 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
   $scope.loginData = {};
 
-  // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
 
-  // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
   };
 
-  // Open the login modal
   $scope.login = function() {
     $scope.modal.show();
   };
 
-  // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
     $timeout(function() {
       $scope.closeLogin();
     }, 1000);
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('CachorroCtrl', function($scope) {
+  $scope.titulo="Busca por Cachorros";
+  $scope.animais = [{
+    "nome":"Thor",
+    "descricao":"Pensando mais a longo prazo. o inicio da atividade de formação",
+    "contato":"2345678",
+    "imagem":"http://lorempixel.com/300/150/animais/55"
+  },
+  {
+    "nome":"Eike",
+    "descricao":"Pensando mais a longo prazo. o inicio da atividade de formação",
+    "contato":"321321321",
+    "imagem":"http://lorempixel.com/300/150/animais/55"
+  },
+  {
+    "nome":"Cunha",
+    "descricao":"Pensando mais a longo prazo. o inicio da atividade de formação",
+    "contato":"12340987",
+    "imagem":"http://lorempixel.com/300/150/animais/55"
+  },
+  {
+    "nome":"Aecia",
+    "descricao":"Pensando mais a longo prazo. o inicio da atividade de formação",
+    "contato":"54678342",
+    "imagem":"http://lorempixel.com/300/150/animais/55"
+  }];
+})
+
+.controller('GatosCtrl', function($scope) {
+  $scope.animais = [
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('PerfilCtrl', function($scope) {
 });
